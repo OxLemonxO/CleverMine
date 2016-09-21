b32368b145f1f5beaa5a0b42d462c57fa338d836
Mafrans K <malte.kluft@gmail.com>
2016-09-21 22:22:21 +0200

    Add more things

---
+++ src/me/oxlemonxo/clevermine/data/Lists.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package me.oxlemonxo.clevermine.data;
+import java.util.*;
+
+import me.oxlemonxo.clevermine.data.*;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author malmar03
+ */
+public class Lists 
+{
+    public static List<Player> botUsers = new ArrayList();
+    public static HashMap<Player, Boolean> userPublic = new HashMap();
+}
---
+++ src/me/oxlemonxo/clevermine/data/Strings.java
@@ -0,0 +1,9 @@
+package me.oxlemonxo.clevermine.data;
+
+import org.bukkit.ChatColor;
+
+
+public class Strings 
+{
+    public static String senderMessageFormat = ChatColor.YELLOW + "["  + ChatColor.RED + "Clever" + ChatColor.GOLD + "Mine" + ChatColor.YELLOW + "]" + ChatColor.GRAY + "%s";
+}

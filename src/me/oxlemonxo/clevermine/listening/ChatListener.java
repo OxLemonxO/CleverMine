a23f4badc9324e3feefe52fd29c289ff3fa412a1
Lemon <minecraftoxlemonxo@gmail.com>
2016-09-22 12:53:24 +0500

    Update ChatListener.java

--- src/me/oxlemonxo/clevermine/listening/ChatListener.java
+++ src/me/oxlemonxo/clevermine/listening/ChatListener.java
@@ -1,13 +1,13 @@
 package me.oxlemonxo.clevermine.listening;
 
-import me.oxlemonxo.clevermine.CleverMine;
+import static me.oxlemonxo.clevermine.CleverMine.plugin;
 import me.oxlemonxo.clevermine.Log;
 import me.oxlemonxo.clevermine.PlayerData;
 import me.oxlemonxo.clevermine.cleverbot.ChatterBot;
 import me.oxlemonxo.clevermine.cleverbot.ChatterBotFactory;
 import me.oxlemonxo.clevermine.cleverbot.ChatterBotSession;
 import me.oxlemonxo.clevermine.cleverbot.ChatterBotType;
-import net.md_5.bungee.api.ChatColor;
+import org.bukkit.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -21,14 +21,15 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class ChatListener implements Listener
 {
 
-    private CleverMine plugin = CleverMine.plugin;
-
     @EventHandler(priority = EventPriority.NORMAL)
     public void onPlayerChat(AsyncPlayerChatEvent event)
     {
         Player player = event.getPlayer();
-        String message = event.getMessage();
-        if (message.startsWith(plugin.config.getString("bot.trigger").replace("%botname%", ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name"))))))
+        String trigger = plugin.config.getString("bot.trigger").replace("&", "").replace("%botname%", plugin.config.getString("bot.name"));
+        String msg = event.getMessage().toLowerCase();
+        String message = event.getMessage().replace(trigger, "");
+
+        if (msg.startsWith(trigger.toLowerCase()))
         {
             new BukkitRunnable()
             {
@@ -40,42 +41,38 @@ public class ChatListener implements Listener
                         PlayerData data = PlayerData.getPlayerData(player);
                         if (data.isEnabled())
                         {
-                            String msg = message.replace(plugin.config.getString("bot.trigger").replace("%botname%", ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name")))).toLowerCase(), "");
+
                             ChatterBotFactory factory = new ChatterBotFactory();
                             ChatterBot bot = factory.create(ChatterBotType.CLEVERBOT);
                             ChatterBotSession session = bot.createSession();
                             if (data.isPersonal())
                             {
-                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.formatting").replace("%botname%", ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name"))).replace("%message%", session.think(msg))));
-                            } 
-                            else
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.formatting").replace("%botname%", ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name"))).replace("%message%", session.think(message))));
+                            } else
                             {
 
-                                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.formatting").replace("%botname%", ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name"))).replace("%message%", session.think(msg))));
+                                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.formatting").replace("%botname%", ChatColor.translateAlternateColorCodes('&', plugin.config.getString("bot.name"))).replace("%message%", session.think(message))));
                             }
                         }
-                    } 
-                    catch (Exception ex)
+                    } catch (Exception ex)
                     {
                         Log.severe(ex);
                     }
                 }
-            }.runTaskAsynchronously(CleverMine.plugin);
+            }.runTaskAsynchronously(plugin);
         }
-
     }
-    
+
     @EventHandler
     public void onCommandPreprocess(PlayerCommandPreprocessEvent e)
     {
         Player player = e.getPlayer();
         String command = e.getMessage();
-        
-        if(plugin.config.getStringList("commandaliases").contains(command.replaceFirst("/", "").split(" ")))
+
+        if (plugin.config.getStringList("commandaliases").contains(command.replaceFirst("/", "").split(" ")))
         {
-            plugin.getServer().dispatchCommand((CommandSender)player, "clevermine " + command.replaceFirst("/", "").replaceFirst(command.split(" ")[0], ""));
+            plugin.getServer().dispatchCommand((CommandSender) player, "clevermine " + command.replaceFirst("/", "").replaceFirst(command.split(" ")[0], ""));
             e.setCancelled(true);
         }
     }
 }
-

9b74375f30c01f1fb401ee89bb38a39a8e09a0fc
Lemon <minecraftoxlemonxo@gmail.com>
2016-09-22 12:54:43 +0500

    Update Command_clevermine.java

--- src/me/oxlemonxo/clevermine/commands/Command_clevermine.java
+++ src/me/oxlemonxo/clevermine/commands/Command_clevermine.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
 
-@CommandPermissions(source = SourceType.ONLY_IN_GAME)
+@CommandPermissions(source = SourceType.BOTH)
 @CommandParameters(description = "Main command", usage = "/<command> <on | off | personal | global>")
 public class Command_clevermine extends BaseCommand
 {
@@ -14,23 +14,24 @@ public class Command_clevermine extends BaseCommand
     @Override
     public boolean run(final CommandSender sender, final Player sender_p, final Command cmd, final String commandLabel, final String[] args, final boolean senderIsConsole)
     {
+        PlayerData data = PlayerData.getPlayerData(sender_p);
+        String botPrefix = "[" + plugin.config.getString("bot.name") + "]";
+
         if (args.length != 1)
         {
             return false;
         }
-        PlayerData data = PlayerData.getPlayerData(sender_p);
-        switch (args[0])
+        switch (args[0].toLowerCase())
         {
-
             case "on":
             {
                 if (data.isEnabled())
                 {
-                 sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Bot is already enabled for you.");
-                 return true;
+                    sender.sendMessage(botPrefix + " Bot is already enabled for you.");
+                    return true;
                 }
                 data.enable();
-                sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Bot has been successfully enabled for you.");
+                sender.sendMessage(ChatColor.BLUE + botPrefix + " Bot has been successfully enabled for you.");
                 return true;
             }
 
@@ -38,40 +39,42 @@ public class Command_clevermine extends BaseCommand
             {
                 if (!data.isEnabled())
                 {
-                 sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Bot is already disabled for you.");
-                 return true;
+                    sender.sendMessage(ChatColor.BLUE + botPrefix + " Bot is already disabled for you.");
+                    return true;
                 }
                 data.disable();
-                sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Bot has been successfully disabled for you.");
+                sender.sendMessage(ChatColor.BLUE + botPrefix + " Bot has been successfully disabled for you.");
                 return true;
             }
 
             case "personal":
             {
-                if(data.isPersonal()) 
+                if (data.isPersonal())
                 {
-                  sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Mode is already personal.");
-                  return true;
+                    sender.sendMessage(ChatColor.BLUE + botPrefix + " Mode is already personal.");
+                    return true;
                 }
                 data.setPersonal();
-                sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Mode set to personal.");
+                sender.sendMessage(ChatColor.BLUE + botPrefix + " Mode set to personal.");
                 return true;
             }
 
             case "global":
             {
-                if(!data.isPersonal())
+                if (!data.isPersonal())
                 {
-                    sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Mode is already global.");
+                    sender.sendMessage(ChatColor.BLUE + botPrefix + " Mode is already global.");
+                    return true;
                 }
                 data.setGlobal();
-                sender.sendMessage(ChatColor.BLUE + plugin.config.getString("bot.name") + " Mode set to global.");
+                sender.sendMessage(ChatColor.BLUE + botPrefix + " Mode set to global.");
+                return true;
             }
-            
+
             default:
             {
                 return false;
-            }     
+            }
         }
     }
 }
